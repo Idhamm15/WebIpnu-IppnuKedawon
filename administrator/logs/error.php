e12e4578b5ee80cd3f7393cc5011d4cdbe95419f
Idhamm15 <idhamtamvanz123@gmail.com>
2022-07-06 14:41:25 +0700

    Web Profile Ipnu-Ippnu Cabang Kedawon Brebes

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-12-30 16:25:59 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-12-30T16:25:58+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-12-30T16:56:36+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-12-30T17:16:58+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/upload.error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-01-01 05:22:08 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-01-01T05:22:08+00:00	INFO ::1	upload	
+2022-01-01T06:56:45+00:00	INFO ::1	upload	
